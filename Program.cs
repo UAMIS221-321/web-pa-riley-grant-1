c7d822ea3c00853863749c090786facc28f1473e
riley <rggrant1@crimson.ua.edu>
2022-04-30 17:25:12 -0500

    final commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace web_pa_riley_grant_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
